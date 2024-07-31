1d74590e5a5a82ece5bc01a40184679298eb005d
Doanmaiha <doanmaiha1502@gmail.com>
2024-07-31 18:05:56 +0700

    Setup

---
+++ GamePanel.java
@@ -0,0 +1,20 @@
+package TetrisJava;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel {
+
+    public static final int WIDTH = 1280;
+    public static final int HEIGHT = 720;
+
+    public GamePanel() {
+        // Panel Settings
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        this.setBackground(Color.black);
+        this.setLayout(null);
+    }
+    
+}
