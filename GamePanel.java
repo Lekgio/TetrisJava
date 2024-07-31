e110202a480d9d0e47fec3486e2c5ca8c9f1df1b
Doanmaiha <doanmaiha1502@gmail.com>
2024-07-31 20:13:14 +0700

    Play Area

--- GamePanel.java
+++ GamePanel.java
@@ -3,6 +3,7 @@ package TetrisJava;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
@@ -12,12 +13,15 @@ public class GamePanel extends JPanel implements Runnable{
     public static final int HEIGHT = 720;
     final int FPS = 60;
     Thread gameThread;
+    PlayManager pm;
 
     public GamePanel() {
         // Panel Settings
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         this.setBackground(Color.black);
         this.setLayout(null);
+
+        pm = new PlayManager();
     }
 
     public void launchGame() {
@@ -49,9 +53,12 @@ public class GamePanel extends JPanel implements Runnable{
     }
 
     private void update() {
-
+        pm.update();
     }
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+        pm.draw(g2);
     }
 }
